b7aade3696c901d619637bb3fa3d3026f121abf4
Jacob Deepak <jacobdeepak@MacBook-Pro.local>
2019-02-19 12:41:58 -0500

    feat: added support for google login.

---
+++ server/src/data/dataaccess.go
@@ -0,0 +1,87 @@
+package data
+
+import (
+	"context"
+	"time"
+
+	"../cassandra"
+	"../logger"
+
+	"github.com/gocql/gocql"
+	"github.com/google/uuid"
+)
+
+const (
+	host              = "127.0.0.1:9042"
+	keySpace          = "employee"
+	connectionTimeout = 10 * time.Second
+)
+
+var (
+	logCtx = context.Background()
+)
+
+type Emp struct {
+	Empno    gocql.UUID
+	Ename    string
+	Job      string
+	Mgr      gocql.UUID
+	Hiredate time.Time
+	Sal      string
+	Comm     string
+	Deptno   int
+}
+
+func AccessData() {
+	c := &cassandra.Cassandra{}
+
+	rqId, _ := uuid.NewRandom()
+	rqCtx := logger.WithRqId(logCtx, rqId)
+	log := logger.Logger(rqCtx).Sugar()
+	defer log.Sync()
+
+	log.Infof(
+		"trying to connect - cassandra %s to keyspace %s with timeout %d",
+		host,
+		keySpace,
+		connectionTimeout,
+	)
+
+	c.SetConfig(host, keySpace, connectionTimeout)
+	_, err := c.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Infof(
+		"connected - cassandra %s @ %s",
+		host,
+		keySpace,
+	)
+
+	defer c.Close()
+
+	log.Infof("trying to fetch employees")
+	emps := getAllEmps(c)
+	log.Infof("got %d employees", len(emps))
+}
+
+func getAllEmps(c *cassandra.Cassandra) []Emp {
+	var emps []Emp
+	m := map[string]interface{}{}
+	iter := c.GetSession().Query("SELECT * FROM emp").Iter()
+	for iter.MapScan(m) {
+		emps = append(emps, Emp{
+			Empno:    m["empno"].(gocql.UUID),
+			Ename:    m["ename"].(string),
+			Job:      m["job"].(string),
+			Mgr:      m["mgr"].(gocql.UUID),
+			Hiredate: m["hiredate"].(time.Time),
+			//	Sal:      m["sal"].(string),
+			//	Comm:     m["comm"].(string),
+			//	Deptno:   m["deptno"].(int),
+		})
+		m = map[string]interface{}{}
+	}
+	return emps
+}
